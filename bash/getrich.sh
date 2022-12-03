d5e1b84ee415fc0ef8687e5ae8af86a5ec5ac7d2
Cody Watzen <cwatzen@gmail.com>
2022-12-02 22:56:52 -0600

    added bash scripts and folders

---
+++ bash/getrich.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A bash script that I took from a NetworkChuck tutorial https://www.youtube.com/watch?v=19nN9vgcgmU
+echo "What is your name?"
+read -r name
+echo "How old are you?"
+read -r age
+echo "Hello $name, you are $age years old. Let's find out when you will be a millionaire."
+echo "Calcalating when you will become rich."
+echo "......."
+echo "*......"
+echo "**....."
+echo "*****.."
+echo "*******"
+numRan=$(($RANDOM % 15))
+richAge=$(($numRan + $age))
+ageDif=$(($richAge - $age))
+echo "$name, you will be a millionaire at the age of $richAge. That is $ageDif years away."
---
+++ bash/orgByExt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# A script I created back when learning cmd, I updated that script to powershell and now updated the script to bash. 
+dir=~/Downloads/
+for filename in ~/Downloads/*; do
+    base=${filename%.*}
+    ext=${filename#"$base".}
+    extDir=${dir}${ext}
+    echo "$filename"
+    echo "$base"
+    echo "$ext"
+    if [ -d "$extDir" ]; then
+        echo "Moving $filename to $extDir"
+        mv "${filename}" "${extDir}"
+    else
+        echo "$extDir doesn't exist, creating directory"
+        mkdir "$extDir"
+        echo "Moving $filename to $extDir"
+        mv "$filename" "$extDir"
+    fi
+done
