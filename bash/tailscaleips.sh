11119ae1218061595958aee328d962edf4756c53
Cody Watzen <cwatzen@gmail.com>
2023-04-23 23:25:34 -0500

    created tailscaleips.sh

---
+++ bash/tailscaleips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ipsallowed="/home/{username}/ipsallowed"
+machineTSIP=$(tailscale ip -4)
+tailscalemachines=$(tailscale status | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep -v ">
+for ip in $tailscalemachines; do {
+     if grep -Fxq "$ip" $ipsallowed
+        then 
+          echo "$ip is already in file"
+        else
+          echo $ip >> $ipsallowed
+     fi
+}
+done
+
+for x in `cat $ipsallowed`; do {
+sudo ufw allow from $x
+}
+done
+sudo ufw reload > /dev/null
