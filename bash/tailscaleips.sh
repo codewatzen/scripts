bbe6a8078e5ed91abdc4e41429438d8c2c4a06b7
Cody Watzen <cwatzen@gmail.com>
2023-04-24 22:48:45 -0500

    reformatted two shell scripts

--- bash/tailscaleips.sh
+++ bash/tailscaleips.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-ipsallowed="/home/{username}/ipsallowed"
+ipsallowed="$HOME/ipsallowed"
 machineTSIP=$(tailscale ip -4)
 tailscalemachines=$(tailscale status | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep v "${machineTSIP}")
-for ip in $tailscalemachines; do {
-    if grep -Fxq "$ip" $ipsallowed
-        then 
-          echo "$ip is already in file"
-        else
-          echo "$ip" >> $ipsallowed
+for ip in $tailscalemachines; do
+  {
+    if grep -Fxq "$ip" "$ipsallowed"; then
+      echo "$ip is already in file"
+    else
+      echo "$ip" >>"$ipsallowed"
     fi
-}
+  }
 done
 
-for x in $(cat $ipsallowed); do {
-sudo ufw allow from "$x"
-}
+for x in $(cat $ipsallowed); do
+  {
+    sudo ufw allow from "$x"
+  }
 done
-sudo ufw reload > /dev/null
+sudo ufw reload >/dev/null
