581fe8f7d9c0222fb2e9ddbb0127585d0a1fe63e
Cody Watzen <cwatzen@gmail.com>
2023-05-30 12:01:59 -0500

    updated file

--- bash/plexinteractivefileinfo-pt.sh
+++ bash/plexinteractivefileinfo-pt.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+function movies() {
+    videodirs="${plex}/movies"
+    csv="${HOME}/moviefilesinfo.csv"
+    echo "Size (GB);File Name;Resolution;Codec" >"${csv}"
+    fdfind -t f -E "*.gz" -E "*.srt" -E "*.sub" . ${videodirs} \
+        -x ffprobe -v quiet -print_format json -show_format -select_streams v:0 \
+        -show_entries stream \
+        -i | jq --raw-output '. | "=\(.format.size)/1073741824;\((.format.filename | sub(".+(?=\/).";"")));\(.streams[0].width)x\(.streams[0].height);\(.streams[0].codec_name)"' >>"${csv}"
+}
+
 selection() {
-PS3='Which Plex folder do you want to get information from: '
-options=("Anime Movies" "Kid Movies" "Movies" "Sports" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
+    PS3='Which Plex folder do you want to get information from: '
+    options=("Anime Movies" "Kid Movies" "Movies" "Sports" "Quit")
+    select opt in "${options[@]}"; do
+        case $opt in
         "Anime Movies")
             echo "you chose $opt running animovies fuction now."
             break
@@ -24,9 +33,9 @@ do
         "Quit")
             break
             ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+        *) echo "invalid option $REPLY" ;;
+        esac
+    done
 }
 
 selection
