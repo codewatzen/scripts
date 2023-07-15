8c35471f1f110a4bca1d3d4184187b9c37a2a87f
Cody Watzen <cwatzen@gmail.com>
2023-07-15 12:19:36 -0500

    Optimized & added comments to orgBy & getvideo

--- bash/getvideofileinfo.sh
+++ bash/getvideofileinfo.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Bash file I created using fdfind, ffprobe and jq that outputs format size in GB, file name, resolution and codec to a csv
-videodirs="<directorie of files>"
+# Bash script to output video format details to a CSV file using fdfind, ffprobe, and jq
+videodirs="<directory of files>"
 csv="<location of csv>"
-echo "Size (GB);File Name;Resolution;Codec" >"${csv}"
-fdfind -t f -E "*.gz" -E "*.srt" -E "*.sub" . "${videodirs}" \
-    -x ffprobe -v quiet -print_format json -show_format -select_streams v:0 \
-    -show_entries stream \
-    -i | jq --raw-output '. | "=\(.format.size)/1073741824;\((.format.filename | sub(".+(?=\/).";"")));\(.streams[0].width)x\(.streams[0].height);\(.streams[0].codec_name)"' >>"${csv}"
+
+# Define the header line of the CSV file
+header="Size (GB);File Name;Resolution;Codec"
+
+# Write the header to the CSV file
+echo "$header" >"$csv"
+
+# Find video files using fdfind, run ffprobe, extract relevant details using jq, and append to the CSV file
+fdfind -t f -E "*.gz" -E "*.srt" -E "*.sub" . "$videodirs" \
+    -exec bash -c "ffprobe -v quiet -print_format json -show_format -select_streams v:0 -show_entries stream -i \"$0\" |
+    jq --raw-output '. | \"=\(.format.size)/1073741824;\((.format.filename | sub(\".+(?=\/).\";\"\")));\(.streams[0].width)x\(.streams[0].height);\(.streams[0].codec_name)\"'" {} >>"$csv"
--- bash/orgByExt.sh
+++ bash/orgByExt.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # A script I created back when learning cmd, I updated that script to powershell and now updated the script to bash.
-dir="<Working directory>"
-for filename in "${dir}"/*; do
-    base=${filename%.*}
-    ext=${filename#"$base".}
-    extDir=${dir}${ext}
-    echo "$filename"
-    echo "$base"
-    echo "$ext"
+
+dir=~/Downloads/
+
+# Iterate through each file in the directory
+for filename in "$dir"*; do
+    base=${filename%.*}   # Extract the base name of the file (without extension)
+    ext=${filename##*.}   # Extract the extension of the file
+    extDir="${dir}${ext}" # Create the target directory path using the extension
+
+    echo "$filename" # Print the current file path
+    echo "$base"     # Print the base name of the file
+    echo "$ext"      # Print the file extension
+
     if [ -d "$extDir" ]; then
+        # If the target directory already exists, move the file into it
         echo "Moving $filename to $extDir"
-        mv "${filename}" "${extDir}"
+        mv "$filename" "$extDir"
     else
+        # If the target directory doesn't exist, create it and then move the file into it
         echo "$extDir doesn't exist, creating directory"
-        mkdir "$extDir"
+        mkdir -p "$extDir"
         echo "Moving $filename to $extDir"
         mv "$filename" "$extDir"
     fi
